80ffa7fe597d97cd8cdd946d02437772cf81c9d3
ChrisKabbouchy <57867758+ChrisKabbouchy@users.noreply.github.com>
2020-03-22 13:57:02 +0200

    Initial Commit

---
+++ KabNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KabNews
+//
+//  Created by Christian Kabouchy on 3/22/20.
+//  Copyright © 2020 Christian Kabouchy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
